32f7ebb4675286e72675a9d01f59f59a52104d5f
CebL2 <sliu@connect.kellettschool.com>
2021-04-14 09:44:01 +0800

    function

--- test.py
+++ test.py
@@ -2,3 +2,11 @@ print("helloworld")
 
 #this is a test to see whether if the change is applied to github
 #test part 2    
+
+
+def test():
+    thing = 1
+    if thing == 0:
+        print("No")
+    else:
+        print("Yes")
