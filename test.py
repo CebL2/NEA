0256dee2e9cef2f8b5fdc7252a2161699da9c34b
CebL2 <sliu@connect.kellettschool.com>
2021-04-25 19:01:18 +0800

    screen test

--- test.py
+++ test.py
@@ -1,9 +1,20 @@
-print("helloworld")
-
+import pygame
 #this is a test to see whether if the change is applied to github
 #test part 2    
 
+running = True
+while running:
+    screen.fill((255, 255, 255)) #the colour
+    
+    
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT: # Did the user click the window close button?
+            running = False
+#Menu
 
+    
+    
+    
 def test():
     thing = 1
     if thing == 0:
@@ -11,6 +22,6 @@ def test():
     else:
         print("Yes")
     
-print(test())
+#print(test())
         
 
