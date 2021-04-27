7c2a109145f9cad2fd0d458b586cccfa35e46912
CebL2 <sliu@connect.kellettschool.com>
2021-04-27 12:44:09 +0800

    more buttons

--- test.py
+++ test.py
@@ -9,19 +9,47 @@ white = (255,255,255)
 Black = (0,0,0)
 
 print(pygame.font.match_font('impact'))
-    
- 
-MenuKey = pygame.key.get_pressed()
-click = False
 
+textfont = pygame.font.Font(r'C:\Users\user\Desktop\Computer Science\impact.ttf',32 )
+ 
+click = False
+test = textfont.render("test", True, white)
 screen = pygame.display.set_mode((1280,1024 ))
-running = True  
-def testMenu():
+running = True
+
+
+
+
+
+def anothermenu():
     running = True
     while running:
+        
         screen.fill(white)
-        button2 = pygame.draw.rect(screen, Black,(100,100,50,50))
+        
+        screen.blit(textfont.render("options",True, Black), (200,50))
+        button3 = pygame.draw.rect(screen, Black,(100,100,50,50))
+        screen.blit(test, (50,50))
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT: # Did the user click the window close button?
+                running = False
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_ESCAPE:
+                    running = False
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                click = True
+        pygame.display.update()
+        
 
+
+def testMenu():
+    running = True
+   
+    
+    while running:
+        screen.fill(white)
+        button3 = pygame.draw.rect(screen, Black,(100,100,50,50))
+        screen.blit(test, (50,50))
         for event in pygame.event.get():
             if event.type == pygame.QUIT: # Did the user click the window close button?
                 running = False
@@ -36,12 +64,16 @@ def testMenu():
 
 while running:
     screen.fill(white) #the colour
-    #screen.blit(top_text, (250, 250))
     button1 = pygame.draw.rect(screen, Black,(200,500,20,500))
     mousex, mousey = pygame.mouse.get_pos()
     if button1.collidepoint(mousex,mousey):
         if click == True:
             testMenu()
+            click = False
+    button2 = pygame.draw.rect(screen, Black,(200,50,500,500))
+    if button2.collidepoint(mousex,mousey):
+        if click == True:
+            anothermenu()
             click = False
             
     
@@ -61,18 +93,3 @@ while running:
         if event.type == pygame.MOUSEBUTTONDOWN:
             click = True
     pygame.display.update()
-
-
-
-
-
-def test():
-    thing = 1
-    if thing == 0:
-        print("No")
-    else:
-        print("Yes")
-    
-#print(test())
-        
-
